7499dc037a26a87cc703fc63dfc4e1e85d9d9f24
anirbandsharma <85803381+anirbandsharma@users.noreply.github.com>
2021-12-27 10:10:42 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "jec_main";
+$database = "jec";
 
 $con=mysqli_connect($server,$username,$password,$database)or die("can't connect...");
 ?>
